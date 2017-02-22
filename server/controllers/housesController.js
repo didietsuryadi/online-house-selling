710b41e9c1b5cfbf2535c817fe5e6e5d66de8d34
didietsuryadi <didietsuryadi>
2017-02-22 13:24:30 +0700

    finish-crud-spa

--- server/controllers/housesController.js
+++ server/controllers/housesController.js
@@ -22,8 +22,13 @@ module.exports = {
       res.send(data)
     })
   },
-  updateQuantity: function(req,res) {
-    House.find({_id: req.body.id},{
+  getHouseById: function(req,res) {
+    House.findOne({_id:req.params.id}).then(function(data){
+      res.send(data)
+    })
+  },
+  updateHouse: function(req,res) {
+    House.findOneAndUpdate({_id: req.params.id},{
       title: req.body.title,
       fullname: req.body.fullname,
       phonenumber: req.body.phonenumber,
@@ -34,6 +39,12 @@ module.exports = {
       lat: req.body.lat
     },{new: true}).then(function(data){
       res.send(data)
+    })
+  },
+  deleteHouse: function(req,res){
+    House.findOneAndRemove({_id: req.params.id})
+    .then(function(data){
+      res.send('House Deleted')
     })
   }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,12 @@ router.get('/', function (req,res,next) {
 
 router.get('/api/houses', controller.getAllHouses);
 
+router.get('/api/houses/:id', controller.getHouseById);
+
 router.post('/api/houses', controller.newHouses);
+
+router.put('/api/houses/:id', controller.updateHouse);
+
+router.delete('/api/houses/:id', controller.deleteHouse);
 
 module.exports = router;
